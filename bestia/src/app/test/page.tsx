c5ac5697fa1f5ef7d56b3102c8faec378b3b57d1
dtic sistemas spa <psaldivia@dtichile.cl>
2026-05-26 21:01:29 -0400

    Add test page

---
+++ bestia/src/app/test/page.tsx
@@ -0,0 +1,7 @@
+export default function TestPage() {
+  return (
+    <div className="flex items-center justify-center min-h-screen">
+      <h1 className="text-4xl font-bold">Test Page Works!</h1>
+    </div>
+  );
+}
